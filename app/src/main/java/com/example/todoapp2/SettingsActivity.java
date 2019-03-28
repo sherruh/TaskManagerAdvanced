0386d80fb5d7c04ff0080a0ed3fead368592a868
sherruh <sherruh@gmail.com>
2019-03-28 23:37:35 +0600

    selecting color

--- app/src/main/java/com/example/todoapp2/SettingsActivity.java
+++ app/src/main/java/com/example/todoapp2/SettingsActivity.java
@@ -11,18 +11,47 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.bumptech.glide.load.resource.drawable.DrawableResource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    List<ImageView> imageViewList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        imageViewList=new ArrayList<>();
+        imageViewList.add((ImageView)findViewById(R.id.theme1_image));
+        imageViewList.add((ImageView)findViewById(R.id.theme2_image));
+        imageViewList.add((ImageView)findViewById(R.id.theme3_image));
+        imageViewList.add((ImageView)findViewById(R.id.theme4_image));
+        imageViewList.add((ImageView)findViewById(R.id.theme5_image));
+        imageViewList.add((ImageView)findViewById(R.id.theme6_image));
+        imageViewList.add((ImageView)findViewById(R.id.theme7_image));
+        imageViewList.add((ImageView)findViewById(R.id.theme8_image));
+
     }
 
     public void onClickTheme(View view) {
         LayoutInflater layout=getLayoutInflater();
         ImageView imageView= (ImageView) view;
-        ColorDrawable drawable = (ColorDrawable) imageView.getBackground();
-        getConte
+        deselctAllImages();
+        imageView.setPadding(10,10,10,10);
+
+    }
+
+    private void deselctAllImages(){
+        for(ImageView imageView:imageViewList){
+            imageView.setPadding(0,0,0,0);
+        }
+    }
+
+    public void onClickGrid(View view) {
+
     }
 }
